e689dc5f1fddbd871f4b2be1c61fe19c37c32501
Carlos Cordoba <carloscba1@gmail.com>
2018-01-05 11:18:09 -0300

    - propTypes para locale

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import Layout from '../components/Layout.js'
 import Style from '../templates/pages/index'
-import appLocale from 'appolodoro-locale'
 
 class Index extends Component {
-
     constructor(props){
         super(props)
-        this.copy = appLocale('es-ES','index', '../../../locale') 
+        this.copy = this.props.copy['es_ES']
     }
 
     showCopy = () => {
-        console.log(this.copy)
+        this.copy = this.props.copy['en_US']
     }
 
     render(){
+        
         return (
             <Layout>
-                <h1>{ (this.copy) ? this.copy.title : '--'}</h1>
+                <h1>{ this.copy.title }</h1>
                 <button onClick={ this.showCopy }>Show</button>
-                <Style />
             </Layout>
         )
     }
+}
+
+const copy = {
+    en_US : {
+        title : <h1>Title</h1>
+    },
+    es_ES:{
+        title : <h1>Titulo</h1>
+    }
+}
+
+Index.propTypes = {
+    copy: PropTypes.object
+};
+
+Index.defaultProps = {
+    copy: copy
 }
 
 export default Index
